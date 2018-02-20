6d297e43e53bbd13ed26f2b4eaea9e8c6dd4a29b
wesratcliff <wes.ratcliff@gmail.com>
2018-02-20 08:26:03 -0800

    Feature/static (#1)
    * feat: App module
    
    * feat: application
    
    * feat: App module
    
    feat: application

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'wes'}
+    posts = [
+        {
+            'author': {'username': 'Author 1'},
+            'body': 'This is the post body'
+        },
+        {
+            'author': {'username': 'Author 2'},
+            'body': 'Another post'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
